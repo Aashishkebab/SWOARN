9b39ff811638724bb8556fd824d7e1d03924e8d2
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2023-12-24 23:46:31 -0500

    Disable anchors when menu is open

--- public/vegan_burger.js
+++ public/vegan_burger.js
@@ -1,3 +1,4 @@
+// Set the onclick for all body elements except the title header space to close the navbar when clicked.
 let sections = document.getElementsByTagName('span');
 for(let i = 0; i < sections.length; i++){
 	if(!(sections[i].id == 'title-span')){
@@ -5,10 +6,32 @@ for(let i = 0; i < sections.length; i++){
 	}
 }
 
+/**
+ * Toggles the state of the navigation menu and disables all anchors if it was just opened.
+ */
 function toggleNavbar(){
-	document.getElementById('vegan-burger-menu').className == 'open-nav' ? document.getElementById('vegan-burger-menu').className = 'closed-nav' : document.getElementById('vegan-burger-menu').className = 'open-nav';
+	document.getElementById('vegan-burger-menu').className == 'open-nav' ? closeNavbar() : document.getElementById('vegan-burger-menu').className = 'open-nav';
+
+	if(document.getElementById('vegan-burger-menu').className == 'open-nav'){
+		let anchors = document.getElementsByTagName('a');
+
+		for(let i = 0; i < anchors.length; i++){
+			anchors[i].style.pointerEvents = 'none';
+			anchors[i].style.cursor = 'default';
+		}
+	}
 }
 
+/**
+ * Closes the navigation menu and re-enables anchors.
+ */
 function closeNavbar(){
 	document.getElementById('vegan-burger-menu').className = 'closed-nav';
+
+	let anchors = document.getElementsByTagName('a');
+
+	for(let i = 0; i < anchors.length; i++){
+		anchors[i].style.pointerEvents = 'auto';
+		anchors[i].style.cursor = 'pointer';
+	}
 }
