9a8c54da504d30a163a33c304773e5d6429d788b
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2023-12-24 23:34:27 -0500

    Yay scripts

---
+++ public/vegan_burger.js
@@ -0,0 +1,14 @@
+let sections = document.getElementsByClassName('text-section');
+for(let i = 0; i < sections.length; i++){
+	if(!(sections[i].id == 'title-span')){
+		sections[i].addEventListener('click', function(){closeNavbar()});
+	}
+}
+
+function toggleNavbar(){
+	document.getElementById('vegan-burger-menu').className == 'open-nav' ? document.getElementById('vegan-burger-menu').className = 'closed-nav' : document.getElementById('vegan-burger-menu').className = 'open-nav';
+}
+
+function closeNavbar(){
+	document.getElementById('vegan-burger-menu').className = 'closed-nav';
+}
