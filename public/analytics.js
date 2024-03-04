968f8a0c0cfb622076e42987b0cba5499c63faf4
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2024-03-03 19:44:21 -0500

    Say hi

--- public/analytics.js
+++ public/analytics.js
@@ -1,10 +1,10 @@
 // Import the functions you need from the SDKs you need
+// https://firebase.google.com/docs/web/setup#available-libraries
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
 
 function logVisit(pageName){
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
+    alert('hi');
 
     // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
