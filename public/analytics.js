61c1e469cf77c6100268fbdae07ec977b25e824c
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2024-03-03 19:36:45 -0500

    Finish analytics

---
+++ public/analytics.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
+
+function logVisit(pageName){
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+        apiKey: "AIzaSyC96Z9qkeRAR7WvUWw5DU1TtyzTCcTleFU",
+        authDomain: "swoarn-42f15.firebaseapp.com",
+        projectId: "swoarn-42f15",
+        storageBucket: "swoarn-42f15.appspot.com",
+        messagingSenderId: "160746800472",
+        appId: "1:160746800472:web:e713895be5611b1a28d12d",
+        measurementId: "G-Q6896MT61Z"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+
+    logEvent(analytics, `${pageName}_visited`);
+}
